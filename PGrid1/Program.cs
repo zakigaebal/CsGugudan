9915259b0b4e5f9bc11f922d9453f2c7e31ae471
zakigaebal <86054791+zakigaebal@users.noreply.github.com>
2022-01-17 16:59:26 +0900

    초기 커밋.

---
+++ PGrid1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gugudan
+
+{
+    // 입력받은 숫자는 : 입력한 변수
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("숫자를 입력하시오");
+
+            string input = Console.ReadLine();
+            int inputNumber = int.Parse(input);
+
+            Console.WriteLine("입력받은 숫자는: " + inputNumber);
+
+            for ( int i = 1; i<=19; i++)
+            {
+                Console.WriteLine(inputNumber+"X" + i + "=" + 2 * i);
+            }
+            Console.ReadLine();
+            
+
+        }
+       
+      }
+          
+        
+    }
+
